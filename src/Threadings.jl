080ed72be0e296a7b0f32885e5e615ea7f583993
James Cook <cookjws@gmail.com>
2018-10-02 15:36:13 +0100

    updated src/Threadings, and added tomls

--- src/Threadings.jl
+++ src/Threadings.jl
@@ -2,13 +2,16 @@ module Threadings
 using Base.Threads
 export tmapreduce, tmap!, tmap
 
-function tmapreduce(f::T, op, v0, itr) where {T<:Function}
+function tmapreduce(f::T, op, itr; init=nothing) where {T<:Function}
   @assert length(itr) > 0
   spinlock = SpinLock()
-  output = deepcopy(v0) # make a deepcopy of starting value
   popable_itr = Vector(deepcopy(itr)) # vector is poppable
+  output = deepcopy(init) # make a deepcopy of starting value
+  if output == nothing
+    output = f(pop!(popable_itr))
+  end
   # array of input arguments to f, to store input for each thread
-  inputs = Vector{typeof(itr[1])}(nthreads()) # deprecated soon?
+  inputs = Vector{typeof(itr[1])}(undef, nthreads()) # deprecated soon?
   @threads for i ∈ eachindex(itr)
     lock(spinlock)
     inputs[threadid()] = pop!(popable_itr)
@@ -26,7 +29,7 @@ function tmap!(f::T, itr, output::U) where {T<:Function, U<:AbstractVector}
   @assert !isempty(itr)
   spinlock = SpinLock()
   all_indices = Vector{Int}(1:length(itr))
-  thread_index = Vector{Int}(nthreads())
+  thread_index = Vector{Int}(undef, nthreads())
   @threads for i ∈ eachindex(itr)
     lock(spinlock)
     thread_index[threadid()] = pop!(all_indices)
@@ -39,7 +42,7 @@ function tmap!(f::T, itr, output::U) where {T<:Function, U<:AbstractVector}
   return nothing
 end
 function tmap(f::T, itr, output_type::Type=Any) where {T<:Function}
-  output = Vector{output_type}(length(itr))
+  output = Vector{output_type}(undef, length(itr))
   tmap!(f, itr, output)
   return output
 end
